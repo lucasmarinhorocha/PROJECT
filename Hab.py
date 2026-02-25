9d3ce7bdefc9bec80fb93227fafb11b80e026580
lucasmarinhorocha <lucasmarinhorocha10@gmail.com>
2026-02-24 23:50:01 -0300

    comeco

---
+++ Hab.py
@@ -0,0 +1,20 @@
+
+class aerotheurge():
+
+    def vento_favoravel(self):
+        print("aumenta o deslocamento em 1,5 metros") # melhor 3 ou 4,5
+    
+    def radiancia_cegante(self):
+        print("recebe X dano aereo e inimigos a 5 metros são considerados cegos a ataques para voce") 
+    
+    def descarga_eletrica(self):
+        print("causa X dano e aplica o statu chocado")
+    def toque_chocante(self):
+
+        print("causa X dano e aplica o statu chocado em corpo a corpo")
+    def rafagada_erratica(self):
+        print("faz o personagem alvo teleportar em uma direção aleatoria quando for atacado, aumenta a resistencia ao ar")
+
+magos_elementais = aerotheurge()
+
+magos_elementais.toque_chocante()
